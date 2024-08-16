fc1e48662d8dd9e3700b6d0786e5610ecc28cbc7
Jeban Christo R <146909816+jeban-christo-2003@users.noreply.github.com>
2024-08-16 23:01:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
-import Scanner as r;
+
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    Scanner view=new Scanner(System.in);
-    int a;
-    System.out.println("Enter the Value of the a");
-    a=view.NextInt();
-
-  }
+ }
 }
